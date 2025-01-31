c8c98d9f08585b37f3c57ba08f8494422f1df36d
am567 <maurya123aman@gmail.com>
2025-02-01 00:38:10 +0530

    commit

---
+++ video54/script.js
@@ -0,0 +1,13 @@
+alert("hello world");
+
+console.log("aman maurya");
+console.log("aman maurya is good ");
+console.log("aman maurya is a good boy");
+
+var a = prompt("Enter your number");
+
+console.log("the value of a is" + a);
+
+document.title = "I am a good boy";
+
+document.body.style.backgroundColor = "red";
