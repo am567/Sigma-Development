96c367e143cf085c259d1775bf9bc245334cc1bb
am567 <maurya123aman@gmail.com>
2025-02-01 16:11:40 +0530

    committed

---
+++ video55/script.js
@@ -0,0 +1,36 @@
+console.log("My name is Aman Maurya");
+
+var a = 9;
+var b = 11;
+
+var c = "aman";
+
+console.log(a + b);
+
+console.log(typeof a, typeof c, typeof b);
+
+// const d = 34; NOT ALLOWED
+// d = d + 1; but it cannot be changed because it is a constant
+
+let x = "Aman Bhai";
+let y = 34;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r);
+
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+let o = {
+
+    "name": "Aman",
+    "Job Code": 5980
+}
+
+console.log(o);
+o.salary = "400 crores";
+console.log(o);
+o.salary = "500 crores";
+console.log(o);
---
+++ video56/index.js
@@ -0,0 +1,15 @@
+console.log("My name is Aman Maurya");
+
+let age = 5;
+let grace = 2;
+
+console.log(age ** grace);
+
+if (age > 8) {
+    console.log("You are not a kid");
+}
+else {
+    console.log("You are a kid");
+}
+
+// here we learn about operators , if else statements and all the types of operators
