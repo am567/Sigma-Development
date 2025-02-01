8b7f343305e40c95941d47eda50b4f0ad955ec36
am567 <maurya123aman@gmail.com>
2025-02-02 00:12:55 +0530

    committed

--- video59/index.js
+++ video59/index.js
@@ -1,37 +1,31 @@
 console.log("Faulty Calculator");
 
+let random = Math.random();
+
+console.log(random);
 
 let a = prompt("Enter the first number: ");
 let b = prompt("Enter the second number: ");
 
 let c = prompt("Enter the operator: ");
 
-function faultycalculator(a, b, operator) {
-    
-    let correctanswer;
 
-    switch (operator) {
-        
-        case '+':
-            correctanswer = a + b;
-            break;
-        
-        case '-':
-            correctanswer = a - b;
-            break;
-        
-        case '*':
-            correctanswer = a * b;
-            break;
-        
-        case '/':
-            correctanswer = a / b;
-            break;
-        
-        default:
-            console.log("Enter a valid operator");
-            break;
-    }
+let obj = {
 
-    
+    "+": "-",            // if the result is true then it will use + operator but if false it will use - operator(when random is less than 0.1)..
+    "-": "/",
+    "*": "+",
+    "/": "**",
 }
+
+if (random > 0.1) {
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`);
+    console.log(`The result is ${a} ${c} ${b}`);
+
+}
+else {
+    c = obj[c];
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`);
+
+}
+
